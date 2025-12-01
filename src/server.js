574a637d9549b8e8b030563dc833b1a737834f3e
Okan Yücel <chaosflisps1@gmail.com>
2025-12-01 22:52:35 +0300

    fix: remove typescript phantom error

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.get("/personel", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
